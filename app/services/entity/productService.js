ddce0e7570f07e7ebcc74f2505b21067450f2f20
Artur Coutinho <arturpc@yahoo.com.br>
2025-11-07 23:26:54 -0300

    PIT-33 - Add new services to product. Fix code in useRequests composable.

--- app/services/entity/productService.js
+++ app/services/entity/productService.js
@@ -14,4 +14,62 @@ export default class ProductService {
         return products.map(product => this.deserialize(product)
         )
     }
+
+    async getProductByUuid(uuid) {
+        const endpoint = `${this.baseUrl}${uuid}`
+        const product = await this.requests.get(endpoint)
+        return this.deserialize(product)
+    }
+
+    deleteProductByUuid(uuid) {
+        const endpoint = `${this.baseUrl}${uuid}`
+        return this.requests.del(endpoint)
+    }
+
+    async createProduct(productData) {
+        const endpoint = `${this.baseUrl}`
+
+        const formData = new FormData()                                                                                                                                           
+        formData.append('name', productData.name)                                                                                                                                 
+        formData.append('description', productData.description)                                                                                                                   
+        formData.append('price', productData.price)
+
+        if (productData.image) {                                                                                                                                                  
+            formData.append('image', productData.image)                                                                                                                           
+        }
+
+        const createdProduct = await this.requests.post(endpoint, formData)
+        return this.deserialize(createdProduct)
+    }
+    
+    async updateProduct(uuid, productData) {
+        const endpoint = `${this.baseUrl}${uuid}`
+
+        const formData = new FormData()                                                                                                                                           
+        formData.append('name', productData.name)                                                                                                                                 
+        formData.append('description', productData.description)                                                                                                                   
+        formData.append('price', productData.price)                                                                                                                               
+                                                                                                                                                                                
+        if (productData.image) {                                                                                                                                                  
+            formData.append('image', productData.image)                                                                                                                           
+        }
+
+        const updatedProduct = await this.requests.put(endpoint, formData)
+        return this.deserialize(updatedProduct)
+    }
+
+    async uploadProductImage(uuid, imageFile) {
+        const endpoint = `${this.baseUrl}${uuid}/upload-image`
+
+        const formData = new FormData()
+        formData.append('image', imageFile)
+
+        const uploadedProduct = await this.requests.post(
+            endpoint, 
+            formData,
+            this.requests.responseTypeOptions.JSON
+        )
+        return this.deserialize(uploadedProduct)
+    }
+
 }
