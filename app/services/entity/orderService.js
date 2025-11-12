1b7ccd4daa6780ea546201cfefbb6d93e561e4b2
Artur Coutinho <arturpc@yahoo.com.br>
2025-11-12 18:56:56 -0300

    PIT-38 - Add Order service

---
+++ app/services/entity/orderService.js
@@ -0,0 +1,40 @@
+import { useRequests } from '@/composables/useRequests'
+import { useMapEndpointResponse } from '~/composables/useMapEndpointResponse'
+
+export default class OrderService {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl
+        this.requests = useRequests()
+        this.deserialize = useMapEndpointResponse().mapOrderFromEndpointBody
+    }
+
+    async getAllOrders() {
+        const endpoint = `${this.baseUrl}`
+        const orders = await this.requests.get(endpoint)
+        return orders.map(order => this.deserialize(order))
+    }
+
+    async getOrderByUuid(uuid) {
+        const endpoint = `${this.baseUrl}${uuid}`
+        const order = await this.requests.get(endpoint)
+        return this.deserialize(order)
+    }
+
+    async createOrder(orderData) {
+        const endpoint = `${this.baseUrl}`
+        const createdOrder = await this.requests.post(endpoint, orderData)
+        return this.deserialize(createdOrder)
+    }
+
+    async updateOrder(uuid, orderData) {
+        const endpoint = `${this.baseUrl}${uuid}`
+        const updatedOrder = await this.requests.put(endpoint, orderData)
+        return this.deserialize(updatedOrder)
+    }
+
+    deleteOrder(uuid) {
+        const endpoint = `${this.baseUrl}${uuid}`
+        return this.requests.del(endpoint)
+    }
+
+}
